94a5ecb0bc9fc9a4662e4fdf4e75f7fcbc122ce3
umetsu <umetsu.kentaro@adways.net>
2023-10-05 12:33:44 +0900

    Vue3 フロントエンド開発の教科書 3.2 の写経

--- hello-vue/src/App.vue
+++ hello-vue/src/App.vue
@@ -1,47 +1,26 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from 'vue'
+
+const now = new Date()
+const nowStr = now.toLocaleTimeString()
+
+let timeStr = nowStr
+const timeStrRef = ref(nowStr)
+
+function changeTime(): void {
+  const newTime = new Date()
+  const newTimeStr = newTime.toLocaleTimeString()
+
+  timeStr = newTimeStr
+  timeStrRef.value = newTimeStr
+}
+
+setInterval(changeTime, 1000)
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <p>timeStr: {{ timeStr }}</p>
+    <p>timeStrRef: {{ timeStrRef }}</p>
+  </div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
